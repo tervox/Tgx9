a78ba111ff5343e1a2a85ef39b2e3251c8b18824
tervox <marcosnozz@hotmail.com>
2026-03-19 20:41:13 -0300

    fix: remove chamadas duplicadas dos scripts originais

--- build_native.sh
+++ build_native.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 set -e
 
-NDK="$ANDROID_SDK_ROOT/ndk/23.2.8568313"
-PREBUILT="$NDK/toolchains/llvm/prebuilt/linux-x86_64"
+NDK_VERSION="23.2.8568313"
+NDK="$ANDROID_SDK_ROOT/ndk/$NDK_VERSION"
+BUILD_PLATFORM="linux-x86_64"
+PREBUILT="$NDK/toolchains/llvm/prebuilt/$BUILD_PLATFORM"
 SYSROOT="$PREBUILT/sysroot"
 THIRDPARTY="$(pwd)/app/jni/third_party"
 CPU_FEATURES="$NDK/sources/android/cpufeatures"
+CPU_COUNT=$(nproc)
 FLAVOR="legacy"
-PLATFORM="armv7-a"
+ABI="armeabi-v7a"
 ANDROID_API=16
 NDK_ABIARCH="armv7a-linux-androideabi"
-CROSS_PREFIX="$PREBUILT/bin/arm-linux-androideabi"
-CC="${PREBUILT}/bin/${NDK_ABIARCH}${ANDROID_API}-clang"
-CXX="${PREBUILT}/bin/${NDK_ABIARCH}${ANDROID_API}-clang++"
-AR="$PREBUILT/bin/llvm-ar"
-NM="$PREBUILT/bin/llvm-nm"
-STRIP="$PREBUILT/bin/llvm-strip"
-RANLIB="$PREBUILT/bin/llvm-ranlib"
-CPU_COUNT=$(nproc)
+TARGET="armv7-android-gcc --enable-neon --disable-neon-asm"
+CPU=armv7-a
+CFLAGS_="-DANDROID -fpic -fpie"
 
-echo "=== Building libvpx for $FLAVOR/$PLATFORM ==="
-pushd "$THIRDPARTY/libvpx"
-make clean || true
-export CC CXX AR NM STRIP RANLIB
-export CFLAGS="-DANDROID -fpic -fpie -Os -march=armv7-a -marm -mfloat-abi=softfp -mfpu=neon -mthumb -D__thumb__ -I${CPU_FEATURES}"
+export PATH=${PREBUILT}/bin:$PATH
+CROSS_PREFIX_CLANG="${PREBUILT}/bin/${NDK_ABIARCH}${ANDROID_API}-"
+
+export AR="${PREBUILT}/bin/llvm-ar"
+export CC="${CROSS_PREFIX_CLANG}clang"
+export CXX="${CROSS_PREFIX_CLANG}clang++"
+export CPP="$CXX"
+export AS="$CC"
+export LD="$CC"
+export STRIP="${PREBUILT}/bin/llvm-strip"
+export RANLIB="${PREBUILT}/bin/llvm-ranlib"
+export NM="${PREBUILT}/bin/llvm-nm"
+
+export CFLAGS="${CFLAGS_} -Os -march=armv7-a -marm -mfloat-abi=softfp -mfpu=neon -mthumb -D__thumb__ -I${CPU_FEATURES}"
+export CPPFLAGS="$CFLAGS"
 export CXXFLAGS="$CFLAGS -std=c++11"
+export ASFLAGS=""
 export LDFLAGS="-L${SYSROOT}/usr/lib"
+PREFIX="./build/$FLAVOR/$CPU"
+
+echo "=== Building libvpx for $FLAVOR/$ABI ==="
+pushd "$THIRDPARTY/libvpx"
+make clean || echo "first time"
+
 ./configure \
-  --target=armv7-android-gcc \
-  --enable-neon \
-  --disable-neon-asm \
-  --enable-runtime-cpu-detect \
   --libc="$SYSROOT" \
-  --prefix="./build/$FLAVOR/$PLATFORM" \
+  --prefix="$PREFIX" \
+  --target="$TARGET" \
+  --enable-runtime-cpu-detect \
   --as=auto \
   --disable-docs \
   --enable-pic \
@@ -49,24 +62,28 @@ export LDFLAGS="-L${SYSROOT}/usr/lib"
   --disable-tools \
   --disable-debug \
   --disable-unit-tests || exit 1
+
 make -j"$CPU_COUNT" install
 popd
 
-echo "=== Building ffmpeg for $FLAVOR/$PLATFORM ==="
+echo "=== Building ffmpeg for $FLAVOR/$ABI ==="
 pushd "$THIRDPARTY/ffmpeg"
-make clean || true
-LIBVPX_INCLUDE="$THIRDPARTY/libvpx/build/$FLAVOR/$PLATFORM/include"
+make clean || echo "first time"
+
+LIBVPX_INCLUDE="$THIRDPARTY/libvpx/build/$FLAVOR/$CPU/include"
 LIBS_DIR="${PREBUILT}/lib64/clang/12.0.9/lib/linux"
 LINK="$SYSROOT/usr/lib/arm-linux-androideabi/$ANDROID_API"
+CROSS_PREFIX="${PREBUILT}/bin/arm-linux-androideabi-"
+
 ./configure \
-  --prefix="./build/$FLAVOR/$PLATFORM" \
+  --prefix="./build/$FLAVOR/$CPU" \
   --enable-cross-compile \
   --target-os=android \
   --arch=arm \
   --cpu=armv7-a \
   --cc="$CC" --cxx="$CXX" --ld="$CC" \
   --ar="$AR" --nm="$NM" --strip="$STRIP" --ranlib="$RANLIB" \
-  --cross-prefix="$CROSS_PREFIX-" \
+  --cross-prefix="$CROSS_PREFIX" \
   --sysroot="$SYSROOT" \
   --enable-static --disable-shared \
   --disable-programs --disable-doc \
@@ -87,7 +104,9 @@ LINK="$SYSROOT/usr/lib/arm-linux-androideabi/$ANDROID_API"
   --extra-ldflags="-L$LINK -L${LIBS_DIR} -Wl,--fix-cortex-a8" \
   --extra-libs="-lunwind -lclang_rt.builtins-arm-android" \
   --enable-neon --disable-x86asm --disable-asm
+
 make -j"$CPU_COUNT"
 make install
 popd
+
 echo "=== Native build complete ==="
