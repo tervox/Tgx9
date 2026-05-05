9cd12a6be5bad39c890747512e01bdd00cbd01e5
tervox <marcosnozz@hotmail.com>
2026-05-05 00:06:48 -0300

    Fix: manifest e patch atualizados

---
+++ patch_manifest.py
@@ -0,0 +1,17 @@
+import sys
+
+path = 'tgx/app/src/main/AndroidManifest.xml'
+content = open(path).read()
+
+service_declaration = '''
+        <service
+            android:name="org.thunderdog.challegram.telegram.UploadNotificationManager$UploadService"
+            android:foregroundServiceType="dataSync|connectedDevice"
+            android:exported="false" />'''
+
+if 'UploadService' not in content:
+    content = content.replace('</application>', service_declaration + '\n    </application>')
+    open(path, 'w').write(content)
+    print('OK: UploadService added to AndroidManifest')
+else:
+    print('SKIP: UploadService already in AndroidManifest')
